584ef303a54f76a085b4b0b84bc72b8a44043889
ikabir21 <ichankabir@gmail.com>
2022-03-03 01:38:22 +0530

    Procile added

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -11,13 +11,28 @@ import router from "../router.js";
 import handleError from "../middlewares/error.js";
 import sessionMiddleware from "../middlewares/session.js";
 
+const allowlist = [
+  "localhost",
+  "127.0.0.1",
+  "http://locahost:3000",
+  "http://127.0.0.1:3000",
+];
+const options = (req, cb) => {
+  let corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  cb(null, corsOptions);
+};
+
 const configureExpress = (app) => {
   app.use(logger("dev"));
   app.use(compression());
   app.use(fileUpload({ useTempFiles: true }));
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
-  app.use(cors());
   app.use(express.json());
   app.use(express.static("public"));
   app.use(
@@ -28,6 +43,7 @@ const configureExpress = (app) => {
   app.use(sessionMiddleware);
   app.use(passport.initialize());
   app.use(passport.session());
+  app.use(cors(options));
   app.use("/", router);
   app.use(handleError);
 
