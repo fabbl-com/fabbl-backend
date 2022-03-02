fdad68eee743ae46169188b03a9a1ae4b607ef54
ikabir21 <ichankabir@gmail.com>
2022-03-03 02:28:31 +0530

    changes

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -43,7 +43,29 @@ const configureExpress = (app) => {
   app.use(sessionMiddleware);
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use(cors({ origin: "*", credentials: true }));
+  app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+  });
   app.use("/", router);
   app.use(handleError);
 
