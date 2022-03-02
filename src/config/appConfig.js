3c0b9e1d050cac7e68e1f75a7e0075163d4b2da0
ikabir21 <ichankabir@gmail.com>
2022-03-03 02:48:15 +0530

    changes

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -45,7 +45,7 @@ const configureExpress = (app) => {
   app.use(passport.session());
   app.use((req, res, next) => {
     // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
     // Request methods you wish to allow
     res.setHeader(
