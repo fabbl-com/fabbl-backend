8a34e225ecd61dd6361de2ee9c1947dd728a0545
ikabir21 <ichankabir@gmail.com>
2022-03-02 22:57:07 +0530

    keys added

---
+++ src/config/keys.js
@@ -0,0 +1,31 @@
+const keys = {
+  google: {
+    clientID:
+      "530928137960-56f38c22fehk4jjrh6ehqprq1v72h03q.apps.googleusercontent.com",
+    clientSecret: "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",
+    callbackURL: "/auth/google/callback",
+  },
+  facebook: {
+    clientID: "328135139191854",
+    clientSecret: "482736380af34c4f2bfad7e0c649570e",
+    callbackURL: "/auth/facebook/callback/",
+  },
+  cloudinary: {
+    cloud_name: "f-12",
+    api_key: "563924833231284",
+    api_secret: "uwRndvuI9gptLSdIbHdF7SoCHKc",
+    PROFILE_URL:
+      "https://res.cloudinary.com/f-12/image/upload/v1644833076/fabbl",
+  },
+  mailingService: {
+    refreshToken:
+      "1//04o4sFz37Qp4WCgYIARAAGAQSNwF-L9IrlNQbWqZ2dmb4trpYW0wqURhXwWqRz8LHx0o0JnYcTDEOu8lrEiMVFduEdwFQuAtZoC0",
+    accessToken:
+      "ya29.A0ARrdaM-28q2fCH05QW9l20pRORdPUoeE3qtyXCiGq-Zg7EtBYL7zk6Wyn82SeSJcb15112ueOwOW2ji1Ns6r3UJ65_SgkPlqB8TOdVxc4gPmN4SL3spxU2Y32VbpSgIAJTawBtEsVslK02fysSHSOqjAMHAM",
+    senderEmail: "ichankabir@gmail.com",
+    redirectURL: "https://developers.google.com/oauthplayground",
+  },
+  clientURL: "http://localhost:3000",
+};
+
+export default keys;
