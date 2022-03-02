b65caa8de4fa39a1683ddc8f3f1acfa4a6a600d4
ikabir21 <ichankabir@gmail.com>
2022-03-03 00:17:06 +0530

    changes

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -11,7 +11,10 @@ import router from "../router.js";
 import handleError from "../middlewares/error.js";
 import sessionMiddleware from "../middlewares/session.js";
 
-const whitelist = ["http://localhost:3000"];
+const whitelist = [
+  "http://localhost:3000",
+  "https://fabbl-backend.herokuapp.com",
+];
 const corsOptions = {
   credentials: true, // This is important.
   origin: (origin, callback) => {
@@ -27,7 +30,15 @@ const configureExpress = (app) => {
   app.use(fileUpload({ useTempFiles: true }));
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
-  app.use(cors(corsOptions));
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
   app.use(express.json());
   app.use(express.static("public"));
   app.use(
